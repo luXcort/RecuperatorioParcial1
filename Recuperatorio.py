9a5bda07dde4b0dda682bfe21d38e63f558190e5
lucas <cortezlucas10@gmail.com>
2025-05-17 20:42:10 -0300

    menu

---
+++ Recuperatorio.py
@@ -0,0 +1,15 @@
+Print("bienvenido al sistema de stock")
+
+#lista
+product=[]
+cant=[]
+
+while true:
+    print("1. Agregar producto")
+    print("2. Ver productos agotados")
+    print("3. Actualizar stock")
+    print("4. Ver todos los productos")
+    print("5. Salir")
+
+    opcion=input("seleccione una opcion del menu")
+
